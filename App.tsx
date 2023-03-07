1a113eed54f26db0b8a71a38c33d78208ba1b1f6
Gaston82 <cabezaartaza@hotmail.com>
2023-03-07 12:39:33 +0100

    Initial commit
    Generated by create-expo-app 1.3.2.

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Eventizi</Text>
+    </View>
+  );
+};
+
+export default App;
