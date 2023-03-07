5d812e4aae58f7dae381d5963328143b7c7117fd
Gaston82 <cabezaartaza@hotmail.com>
2023-03-07 13:59:40 +0100

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,25 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'xo',
-	],
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-	],
-	rules: {
-	},
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "xo", "prettier"],
+  overrides: [
+    {
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+      },
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
 };
