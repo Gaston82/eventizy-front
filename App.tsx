87a220fdb2be55a4cf4bd63f263071e69fa33d7e
Gaston82 <cabezaartaza@hotmail.com>
2023-03-09 12:28:50 +0100

    Add projectId to app.json, install web dependencies for react native

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./src/store/store";
+import { Text, View } from "react-native";
 
 const App = (): JSX.Element => (
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <View style={{ margin: 65 }}>
+      <Text>Hello world </Text>
+    </View>
+  </Provider>
 );
 
 export default App;
