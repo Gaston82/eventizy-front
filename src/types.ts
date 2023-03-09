1a70eb55db976fc6815f865ded8b98cb24d85bed
Gaston82 <cabezaartaza@hotmail.com>
2023-03-09 22:37:08 +0100

    Add environment variable

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,24 @@
+import { type JwtPayload } from "jwt-decode";
 export interface User {
   id: string;
-  email: string;
+  username: string;
   token: string;
 }
 
 export interface UserState extends User {
   isLogged: boolean;
+}
+
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface CustomTokenPayload extends JwtPayload {
+  id: string;
+  username: string;
 }
