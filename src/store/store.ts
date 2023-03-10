4f6e2370c899de5fe0e9a41ea974d9174e346680
Gaston82 <cabezaartaza@hotmail.com>
2023-03-10 10:10:12 +0100

    Add setupStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,22 @@
 import {
+  combineReducers,
   configureStore,
-  type ThunkAction,
-  type Action,
+  type PreloadedState,
 } from "@reduxjs/toolkit";
 import { userReducer } from "./features/userSlice/userSlice";
 
-export const store = configureStore({
-  reducer: { user: userReducer },
+const rootReducer = combineReducers({
+  user: userReducer,
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+
+export const store = setupStore();
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
