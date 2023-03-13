c73d150bb14e18f016e59cd2cef6557605a0d1f2
Gaston82 <cabezaartaza@hotmail.com>
2023-03-13 14:57:20 +0100

    Add showModalAction to useUser hook

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -4,7 +4,7 @@ import decodeToken from "jwt-decode";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { REACT_APP_URL } from "@env";
 import { loginUserActionCreator } from "../../store/features/userSlice/userSlice";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import {
   type LoginResponse,
   type UserCredentials,
@@ -13,28 +13,45 @@ import {
 } from "../../types";
 import {
   setIsLoadingActionCreator,
+  showModalActionCreator,
   unsetIsLoadingActionCreator,
 } from "../../store/features/ui/uiSlice";
 
 const useUser = () => {
   const dispatch = useAppDispatch();
+  const { isError, modalMessage, showModal } = useAppSelector(
+    (state) => state.ui
+  );
 
   const loginUser = async (userInfo: UserCredentials) => {
     dispatch(setIsLoadingActionCreator());
-    const response = await axios.post(`${REACT_APP_URL}/users/login`, userInfo);
 
-    const { token } = (await response.data) as LoginResponse;
+    try {
+      const response = await axios.post(
+        `${REACT_APP_URL}/users/login`,
+        userInfo
+      );
 
-    const { id, username }: CustomTokenPayload = await decodeToken(token);
-    const userLogged: User = {
-      id,
-      token,
-      username,
-    };
-    dispatch(loginUserActionCreator(userLogged));
+      const { token } = (await response.data) as LoginResponse;
 
-    dispatch(unsetIsLoadingActionCreator());
-    await AsyncStorage.setItem("token", token);
+      const { id, username }: CustomTokenPayload = await decodeToken(token);
+      const userLogged: User = {
+        id,
+        token,
+        username,
+      };
+      dispatch(loginUserActionCreator(userLogged));
+      await AsyncStorage.setItem("token", token);
+      dispatch(unsetIsLoadingActionCreator());
+    } catch {
+      dispatch(
+        showModalActionCreator({
+          isError: true,
+          modalMessage: "Something went wrong",
+          showModal: true,
+        })
+      );
+    }
   };
 
   return {
